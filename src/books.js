364466f51cf239c29c243bca391cbefc18357e69
sara faltas <gadly@cloudconceptgroup.com>
2026-04-03 14:51:50 +0200

    resolved

--- src/books.js
+++ src/books.js
@@ -38,14 +38,55 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
+  { title: "The Old Man and the Sea" ,
+pages: 128 ,
+author: "Ernest Hemingway",
+details: {
+ language: "English",
+description: "One of Hemingway's most famous works, it tells the story of Santiago..." ,
+}
+},
+
+{title:"The Airbnb Story" ,
+pages: 256,
+author: "Leight Gallagher",
+details: {
+   language: "English",
+   description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+}
+},
+
+{title: "Educated - A Memoir",
+pages: 352,
+author: "Tara Westover",
+details: {
+   language: "English",
+   description: "Educated is an account of the struggle for self-invention...",
+}
+},
+
+{
+title: "The Art of Learning" ,
+pages: 288 ,
+author: "Josh Waitzkin ",
+details: {
+   language: "English" ,
+   description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure. ",
+}
+}
+
+
+];
 
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(bookObj) {
   // Your code here:
+
+return `${bookObj.title} - ${bookObj.author} - ${bookObj.pages} pages`
 
 }
 
@@ -54,12 +95,23 @@ function getBookDetails() {
 // Iteration 3 | Delete Language
 // Your code here:
 
+for ( let i= 0 ; i< booksArray.length ; i++){
+  delete booksArray[i].details.language;
+}
+console.log(booksArray)
+
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
+//reading time in minutes = (number of pages * 500) / 90
 
+for ( let i= 0 ; i< booksArray.length ; i++){
+  let readingTimeMinutes = ( booksArray[i].pages * 500 )/90
+ booksArray[i].readingTime = Math.ceil(readingTimeMinutes)
+}
+console.log(booksArray)
 
 
 
@@ -86,8 +138,14 @@ const dictionary = {
     ],
 };
 
-function booksByAuthor() {
+
+
+function booksByAuthor(dictionaryObj) {
   // Your code here:
+  // let title = Object.keys(dictionaryObj);
+  //   console.log(title[0])
+  
+
   
 }
 
